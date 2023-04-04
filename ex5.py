cfd7025eee02f13f237ffab65e96028f64f122a9
Zachary Derrick <derrick.zachary@gmail.com>
2023-04-04 17:51:57 +0000

    added metric system and rounded numbers

--- ex5.py
+++ ex5.py
@@ -20,10 +20,12 @@ weight = 177 # pounds
 eyes = "Hazel"
 teeth = "White"
 hair = "Brown"
+convert_height_to_cm = round(height * 2.54)
+convert_weight_to_kg = round(weight / 2.205)
 
 print(f"Let's talk about {name}.")
-print(f"He's {height} inches tall.")
-print(f"He's {weight} pounds heavy.")
+print(f"He's {height} inches tall. That's {convert_height_to_cm} centimeters.")
+print(f"He's {weight} pounds heavy. That's {convert_weight_to_kg} kilograms.")
 print("Actually, that's not too heavy.")
 print(f"He's got {eyes} eyes and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on the coffee.")

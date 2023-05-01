a1285d6d2ef98100acbe7d8049919ed4959865e8
Zachary Derrick <derrick.zachary@gmail.com>
2023-05-01 23:51:59 +0000

    ex9 &9, starting 10

---
+++ ex10.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Script: Printing Printing Printing
+# Author: Zachary Derrick                    
+# Date of latest revision:  2023-4-20    
+# Purpose: Do a more complicated formatting of a string.
+# p.64

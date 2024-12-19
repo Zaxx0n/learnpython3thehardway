8adbd110f97796bc6795bd55b97b3be31a469615
Zachary Derrick <derrick.zachary@gmail.com>
2024-12-19 21:09:53 +0000

    Learning about escape sequences

--- ex10.py
+++ ex10.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 # Script: Printing Printing Printing
 # Author: Zachary Derrick                    
-# Date of latest revision:  2023-4-20    
+# Date of latest revision:  2024-12-19    
 # Purpose: Do a more complicated formatting of a string.
 # p.64
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
